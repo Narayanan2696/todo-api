c0e7620f597d216b987b7d7ba7c3ee89593f24a9
Narayanan V <vnarayanan2696@gmail.com>
2020-01-28 22:28:56 +0530

    serializable response

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"todo-api/structs"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 
+		data := structs.Response{
+			Code: http.StatusOK,
+			Body: "pong",
+		}
+		json.NewEncoder(w).Encode(data)
 		if r.Method == http.MethodGet {
 			fmt.Println("Encountered GET Method")
 		}
