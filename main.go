fa7fd095810778c70e05fcff84af02f987d68247
Narayanan V <vnarayanan2696@gmail.com>
2020-01-28 19:11:34 +0530

    server and response setup

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"structs"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		Json_response := structs.
 
 		if r.Method == http.MethodGet {
 			fmt.Println("Encountered GET Method")
